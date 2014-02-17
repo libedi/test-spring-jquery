766e796a0f79b1188e515f8cabbaf42b91be934c
libedi@gmail.com <libedi@gmail.com@9410c9a3-8a26-b45f-327e-9c57c074dd8e>
2014-02-17 07:48:40 +0000

    get file list

--- testproject1/src/service/TestService.java
+++ testproject1/src/service/TestService.java
@@ -1,9 +1,14 @@
 package service;
 
 import java.sql.SQLException;
+import java.util.List;
+
+import vo.FileVO;
 
 public interface TestService {
 
 	public void fileUpload(String filePath) throws SQLException;
+
+	public List<FileVO> getFileList(int page) throws SQLException;
 
 }
--- testproject1/src/service/TestServiceImpl.java
+++ testproject1/src/service/TestServiceImpl.java
@@ -1,8 +1,10 @@
 package service;
 
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Map;
 
+import vo.FileVO;
 import dao.TestDao;
 
 public class TestServiceImpl implements TestService {
@@ -18,6 +20,12 @@ public class TestServiceImpl implements TestService {
 		// TODO Auto-generated method stub
 		Map param = this.testDao.getUploadParam(filePath);
 		this.testDao.fileUpload(param);
+	}
+
+	@Override
+	public List<FileVO> getFileList(int page) throws SQLException {
+		// TODO Auto-generated method stub
+		return this.testDao.getFileList(page);
 	}
 
 }

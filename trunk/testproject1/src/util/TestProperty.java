e01405802903e310cb37fcb9e9007b227f2198eb
libedi@gmail.com <libedi@gmail.com@9410c9a3-8a26-b45f-327e-9c57c074dd8e>
2014-04-07 09:54:39 +0000

    프로퍼티 테스트

--- trunk/testproject1/src/util/TestProperty.java
+++ trunk/testproject1/src/util/TestProperty.java
@@ -1,22 +1,38 @@
 package util;
 
 import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 
 public class TestProperty {
 	
-	private String FILE = "/test.properites";
-
-	private String getProperty(String keyName){
+	private static final Properties props = new Properties();
+	
+	private String FILE = "test.properties";
+	
+	public String getProperty(String keyName){
+		ClassLoader cl = Thread.currentThread().getContextClassLoader();
+		
+		if(cl == null){
+			cl = ClassLoader.getSystemClassLoader();
+		}
+		URL url = cl.getResource("");
+		
+//		System.out.println(url.getPath());
+		
 		String value = null;
 
 		Properties props = new Properties();
 		FileInputStream fis = null;
 		try {
-			fis = new FileInputStream(FILE);
+			
+//			fis = new FileInputStream(FILE);
+			fis = new FileInputStream(url.getPath() + File.separator + FILE);
+			
 			props.load(new BufferedInputStream(fis));
 			value = props.getProperty(keyName).trim();
 			
@@ -40,12 +56,31 @@ public class TestProperty {
 		return value;
 	}
 	
+	public String loadProperty(String keyName){
+		
+		String value = "";
+		try {		
+			if( getClass().getClassLoader().getResourceAsStream(FILE) != null ) {
+				props.load( getClass().getClassLoader().getResourceAsStream(FILE) );
+			} else {
+				throw new Exception("Property 파일의 위치를 확인할 수 없습니다");			
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		value = props.getProperty(keyName).trim();
+		
+		return value;
+	}
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		TestProperty test = new TestProperty();
+//		System.out.println(test.getProperty("TEST_IP"));
+		System.out.println(test.loadProperty("TEST_IP"));
 	}
 
 }

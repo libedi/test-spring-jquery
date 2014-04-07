55df8d2dca094e33aa79ec1609896cc96e22c076
libedi@gmail.com <libedi@gmail.com@9410c9a3-8a26-b45f-327e-9c57c074dd8e>
2014-04-07 08:52:20 +0000

    암호화코드

--- trunk/testproject1/src/util/EcryptUtil.java
+++ trunk/testproject1/src/util/EcryptUtil.java
@@ -5,7 +5,7 @@ import java.security.NoSuchAlgorithmException;
 
 public class EcryptUtil {
 	
-	public String encryptToSha(String pw){
+	public String encryptToSha1(String pw){
 		byte[] ba = pw.getBytes();
 		MessageDigest md = null;
 		
@@ -26,14 +26,17 @@ public class EcryptUtil {
 			sb.append(Integer.toHexString(0xFF & digest[i]));
 		}
 		System.out.println("PW : " + pw);
-		System.out.print("Byte : ");
-		for(int i=0; i<ba.length; i++){
-			System.out.print(ba[i]);
-		}
 		
 		return sb.toString();
 	}
 	
-	
+	public static void main(String args[]){
+		EcryptUtil eu = new EcryptUtil();
+		String password = "abcd1234";
+		String enc = eu.encryptToSha1(password);
+		System.out.println(enc);
+		System.out.println(enc.length());
+		System.out.println(enc.equals("7ce0359f12857f2a90c7de465f40a95f1cb5da9"));
+	}
 	
 }

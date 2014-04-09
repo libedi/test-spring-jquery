ceeee33702fcdc3518735ec04fa3960e7e9f34bc
libedi@gmail.com <libedi@gmail.com@9410c9a3-8a26-b45f-327e-9c57c074dd8e>
2014-04-09 13:59:34 +0000

    메일 발송 테스트 소스

--- testproject1/src/util/MailUtil.java
+++ testproject1/src/util/MailUtil.java
@@ -1,8 +1,13 @@
 package util;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.MailException;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import vo.MailVO;
 
 public class MailUtil implements SendMailService {
 
@@ -13,13 +18,29 @@ public class MailUtil implements SendMailService {
 	}
 
 	@Override
+	public void sendEmail(MailVO mailVO) {
+		// TODO Auto-generated method stub
+		SimpleMailMessage msg = new SimpleMailMessage();
+		msg.setSubject(mailVO.getSubject());
+		msg.setText(mailVO.getText());
+		msg.setFrom(mailVO.getFromEmail());
+		msg.setTo(mailVO.getToEmail());
+		
+		try{
+			mailSender.send(msg);
+		} catch(MailException e){
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
 	public void sendEmail(String email) {
 		// TODO Auto-generated method stub
 		SimpleMailMessage msg = new SimpleMailMessage();
 		msg.setSubject("테스트 메일");
 		msg.setText("테스트 메일입니다.");
 		msg.setFrom("libedi@gmail.com");
-		msg.setTo("libedi@gmail.com");
+		msg.setTo(email);
 		
 		try{
 			mailSender.send(msg);
